a2671b7ce5c5a234f2b66ae425ad87d07d859c02
Fabian <alexanderfsanchezm@gmail.com>
2022-06-30 17:11:33 -0400

    actualizando datos del carrito

--- carritoweb/carritoweb/urls.py
+++ carritoweb/carritoweb/urls.py
@@ -1,7 +1,7 @@
-"""carritoweb URL Configuration
+"""Carrito URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from home.views import tienda
+from CarritoApp.views import tienda, agregar_producto, eliminar_producto, restar_producto, limpiar_carrito
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', tienda, name="Tienda"),
+    path('agregar/<int:producto_id>/', agregar_producto, name="Add"),
+    path('eliminar/<int:producto_id>/', eliminar_producto, name="Del"),
+    path('restar/<int:producto_id>/', restar_producto, name="Sub"),
+    path('limpiar/', limpiar_carrito, name="CLS"),
 ]
